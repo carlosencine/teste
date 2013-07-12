fed85ff0bfe1db6e031534ef45fd7049050da666
carlosencine <carlos_alexandre88@hotmail.com>
2013-07-12 10:26:46 -0300

    nova alteração de script

--- testeGIT/prog6_11.php
+++ testeGIT/prog6_11.php
@@ -11,5 +11,7 @@ function fatorial($numero) {
 }
 echo "O fatorial de 3 é ".fatorial(3)." <br>";
 echo "O fatorial de 4 é ".fatorial(4)." <br>";
+echo "O fatorial de 15 é ".fatorial(15)." <br>";
+echo "<br>";
 echo "esta linha é somente para demonstrar o uso do github.";
 ?>
